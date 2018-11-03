43706f94887c4df42470d388d0400fff7568f503
charliesands <cwsands3@gmail.com>
2018-11-02 18:06:05 -0600

    layout setup

--- Build An App/MainPage.xaml.cs
+++ Build An App/MainPage.xaml.cs
@@ -26,5 +26,25 @@ namespace Build_An_App
         {
             this.InitializeComponent();
         }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
